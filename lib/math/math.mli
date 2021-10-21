3d80cb526a9d86375c40655fd1a381edc3c4b42f
Iván Pondal <ivan.pondal@gmail.com>
2021-10-21 12:54:56 -0300

    Update doc

--- lib/math/math.mli
+++ lib/math/math.mli
@@ -37,6 +37,6 @@ end
 module Markov : sig
   val absortion_matrix :
     Owl_linalg_d.mat -> Owl_linalg_d.mat -> Owl_linalg_d.mat
-  (** [absortion_matrix q r] calculates the absortion matrix by doing (I - [q])^(-1) x [r].
+  (** [absortion_matrix q r] calculates absortion matrix [b] = (I - [q])^(-1) x [r].
   @return the absortion matrix [b]. *)
 end
