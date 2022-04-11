df9ce8aaab583e58a4f06b07ad143acf26be99b5
Iván Pondal <ivan.pondal@gmail.com>
2022-04-11 11:19:02 -0300

    Add fuse intro

--- diapos/code/FuSe.ml
+++ diapos/code/FuSe.ml
@@ -1,22 +1,22 @@
   (*BEGIN*SumClient*)
-  let sum_client ep x y =
-    let ep = Session.send x ep in
-    let ep = Session.send y ep in
-    let sum, ep = Session.receive ep in
-    Session.close ep;
+  let sum_client ep0 x y =
+    let ep1 = Session.send x ep0 in
+    let ep2 = Session.send y ep1 in
+    let sum, ep3 = Session.receive ep2 in
+    Session.close ep3;
     sum
   (*END*SumClient*)
 
   (*BEGIN*SumServer*)
-  let sum_server ep =
-    let x, ep = Session.receive ep in
-    let y, ep = Session.receive ep in
-    let ep = Session.send (x + y) ep in
-    Session.close ep
+  let sum_server ep0 =
+    let x, ep1 = Session.receive ep0 in
+    let y, ep2 = Session.receive ep1 in
+    let ep3 = Session.send (x + y) ep2 in
+    Session.close ep3
   (*END*SumServer*)
 
   (*BEGIN*SumExample*)
-  let ep1, ep2 = Session.create () in
-  let _ = Thread.create sum_server ep1 in
-  sum_client ep2 40 2
+  let epA, epB = Session.create () in
+  let _ = Thread.create sum_server epA in
+  sum_client epB 40 2
   (*END*SumSumExample*)
