0a1025d7373894d3333fb74bed22dae3af734cb3
Iván Pondal <ivan.pondal@gmail.com>
2022-04-15 12:32:44 -0300

    Add auction code

--- diapos/code/ProFuse.ml
+++ diapos/code/ProFuse.ml
@@ -1,249 +1,43 @@
-module ProbEcho = struct
-  (*BEGIN*SimpleProbEchoService*)
-  let echo_service ep =
-    match Session.branch ep with
-    | `True ep ->
-        let x, ep = Session.receive ep in
-        let ep = Session.send x ep in
-        Session.close ep
-    | `False ep -> Session.idle ep
-  (*END*SimpleProbEchoService*)
-
-  (*BEGIN*SimpleProbEchoClient*)
-  let echo_client ep x =
-    let ep = Session.select_true ep in
-    let ep = Session.send x ep in
-    let x, ep = Session.receive ep in
-    Session.close ep;
-    x
-  (*END*SimpleProbEchoClient*)
-
-  (*BEGIN*SimpleIdleClient*)
-  let idle_client ep =
-    let ep = Session.select_false ep in
-    Session.idle ep
-  (*END*SimpleIdleClient*)
-
-  (*BEGIN*SimpleCoinFlipEchoClient*)
-  let coin_flip_echo_client ep x =
-    Session.pick Rational.one_half
-      (fun ep ->
-        idle_client ep;
-        None)
-      (fun ep -> Some (echo_client ep x))
-      ep
-  (*END*SimpleCoinFlipEchoClient*)
-
-  (*BEGIN*SimpleCoinFlipEchoMain*)
-  let _ =
-    let a, b = Session.create () in
-    let _ = Thread.create echo_server a in
-    coin_flip_echo_client b 42
-  (*END*SimpleCoinFlipEchoMain*)
-
-  (*BEGIN*TwoSessionsPickSingleBranch*)
-  let two_sessions_pick_single_branch epX epY =
-    Session.pick Rational.one_half
-      (fun epX ->
-        let epX = Session.select_false epX in
-        Session.idle epX;
-        let epY = Session.send false epY in
-        Session.close epY)
-      (fun epX ->
-        let epX = Session.select_true epX in
-        Session.close epX)
-      epX
-  (*END*TwoSessionsPickSingleBranch*)
-
-  (*BEGIN*TwoSessionsPickBothBranches*)
-  let two_sessions_pick_both_branches epX epY =
-    Session.pick Rational.one_half
-      (fun epX ->
-        let epX = Session.select_false epX in
-        Session.idle epX;
-        let epY = Session.send false epY in
-        Session.close epY)
-      (fun epX ->
-        let epX = Session.select_true epX in
-        Session.close epX;
-        let epY = Session.send true epY in
-        Session.close epY)
-      epX
-  (*END*TwoSessionsPickBothBranches*)
-
-  (*BEGIN*TwoSessionsInvalidPickBothBranches*)
-  let two_sessions_invalid_pick epX epY =
-    Session.pick Rational.one_half
-      (fun epX ->
-        let epX = Session.select_false epX in
-        Session.idle epX;
-        let epY = Session.send false epY in
-        Session.close epY)
-      (fun epX ->
-        let epX = Session.select_true epX in
-        Session.close epX;
-        let epY = Session.send 42 epY in (* error de tipado *)
-        Session.close epY)
-      epX
-  (*END*TwoSessionsInvalidPickBothBranches*)
-
-  (*BEGIN*TwoSessionsInvalidPickBothBranchesWithSelect*)
-  let two_sessions_invalid_pick epX epY =
-    Session.pick Rational.one_half
-      (fun epX ->
-        let epX = Session.select_false epX in
-        Session.idle epX;
-        let epY = Session.select_false epY in
-        let epY = Session.send false epY in
-        Session.close epY)
-      (fun epX ->
-        let epX = Session.select_true epX in
-        Session.close epX;
-        let epY = Session.select_true epY in (* error de tipado *)
-        let epY = Session.send 42 epY in
-        Session.close epY)
-      epX
-  (*END*TwoSessionsInvalidPickBothBranchesWithSelect*)
-
-  (*BEGIN*TwoSessionsPickTwo*)
-  let two_sessions_valid_pick2 epX epY =
-    Session.pick_2ch Rational.one_half
-      (fun epX epY ->
-        let epX = Session.select_false epX in
-        Session.idle epX;
-        let epY = Session.select_false epY in
-        let epY = Session.send false epY in
-        Session.close epY)
-      (fun epX epY ->
-        let epX = Session.select_true epX in
-        Session.close epX;
-        let epY = Session.select_true epY in
-        let epY = Session.send 42 epY in
-        Session.close epY)
-      epX epY
-  (*END*TwoSessionsPickTwo*)
-
-  (*BEGIN*EchoClientClosedSession*)
-  let run_echo_client_example ?(st = cst_placeholder) () =
-    let ep1, ep2 = Session.create ~st () in
-    let _ = Thread.create echo_service ep1 in
-    echo_client ep2 42
-  (*END*EchoClientClosedSession*)
-
-  (*BEGIN*CoinFlipEchoClientClosedSession*)
-  let run_coin_flip_echo_client_example
-                              ?(st = cst_placeholder) () =
-    let ep1, ep2 = Session.create ~st () in
-    let _ = Thread.create echo_server ep1 in
-    coin_flip_echo_client ep2 42
-  (*END*CoinFlipEchoClientClosedSession*)
-
-  (*BEGIN*PickIdleCloseRunEchoClient*)
-  let pick_idle_close_and_run_echo_client
-                              ?(st = cst_placeholder) () ep =
-    Session.pick Rational.one_half
-      (fun ep ->
-        let ep = Session.select_false ep in
-        Session.idle ep;
-        run_echo_client_example ~st ())
-      (fun ep ->
-        let ep = Session.select_true ep in
-        Session.close ep;
-        run_echo_client_example ~st ())
-      ep
-  (*END*PickIdleCloseRunEchoClient*)
-
-  (*BEGIN*InvalidPickIdleCloseRunEchoClientOrCoinFlip*)
-  let pick_idle_close_and_run_echo_client_or_coin_flip
-                              ?(st = cst_placeholder) () ep =
-    Session.pick Rational.one_half
-      (fun ep ->
-        let ep = Session.select_false ep in
-        Session.idle ep;
-        run_echo_client_example ~st ())
-      (fun ep ->
-        let ep = Session.select_true ep in
-        Session.close ep;
-        match run_coin_flip_echo_client_example ~st () with
-        (* error de tipado *)
-        | Some result -> result
-        | None -> 0)
-      ep
-  (*END*InvalidPickIdleCloseRunEchoClientOrCoinFlip*)
-
-  (*BEGIN*ValidPickIdleCloseRunEchoClientOrCoinFlip*)
-  let pick_idle_close_and_run_echo_client_or_coin_flip
-                              ?(st = cst_placeholder) () ep =
-    Session.pick_2st Rational.one_half
-      (fun ep st ->
-        let ep = Session.select_false ep in
-        Session.idle ep;
-        run_echo_client_example ~st ())
-      (fun ep st ->
-        let ep = Session.select_true ep in
-        Session.close ep;
-        match run_coin_flip_echo_client_example ~st () with
-        | Some result -> result
-        | None -> 0)
-      ep st
-  (*END*ValidPickIdleCloseRunEchoClientOrCoinFlip*)
-
-  (*BEGIN*NoOptionalValidPickIdleCloseRunEchoClientOrCoinFlip*)
-  let pick_idle_close_and_mix_run_echo_client ep =
-    Session.pick Rational.one_half
-      (fun ep ->
-        let ep = Session.select_false ep in
-        Session.idle ep;
-        run_echo_client_example ())
-      (fun ep ->
-        let ep = Session.select_true ep in
-        Session.close ep;
-        match run_coin_flip_echo_client_example () with
-        | Some result -> result
-        | None -> 0)
-      ep
-  (*END*NoOptionalValidPickIdleCloseRunEchoClientOrCoinFlip*)
-
   (*BEGIN*AuctionBuyer*)
   let rec buyer ep offer =
-    let ep = Session.send offer ep in
-    match Session.branch ep with
+    let ep = S.send offer ep in
+    match S.branch ep with
     | `True ep ->
-        Session.close ep;
-        offer
+        (* `Subastador aceptó oferta *)
+        S.close ep
     | `False ep ->
-        let counteroffer, ep = Session.receive ep in
-        Session.pick Rational.two_thirds
+        (* `Subastador rechazó oferta *)
+        let counteroffer, ep = receive ep in
+        pick Rational.two_thirds
           (fun ep ->
-            let ep = Session.select_false ep in
-            buyer ep counteroffer)
+            (* Comprador rechaza contraoferta *)
+            let ep = S.select_false ep in
+            S.idle ep)
           (fun ep ->
-            let ep = Session.select_true ep in
-            Session.idle ep;
-            -1)
+            (* Comprador acepta contraoferta *)
+            let ep = S.select_true ep in
+            buyer ep (counteroffer - 5))
           ep
   (*END*AuctionBuyer*)
 
-  (*BEGIN*AuctionSeller*)
-  let rec seller ep =
-    let bid, ep = Session.receive ep in
-    Session.pick Rational.one_quarter
+  (*BEGIN*Auctioneer*)
+  let rec auctioneer ep =
+    let bid, ep = S.receive ep in
+    pick Rational.one_quarter
       (fun ep ->
-        let ep = Session.select_false ep in
-        let ep = Session.send (bid + 10) ep in
-        match Session.branch ep with
-        | `True ep -> Session.idle ep
-        | `False ep -> seller ep)
+        (* Subastador rechaza oferta *)
+        let ep = S.select_false ep in
+        let ep = S.send (bid + 10) ep in
+        match S.branch ep with
+        | `True ep ->
+          (* Comprador aceptó seguir contraoferta *)
+          auctioneer ep
+        | `False ep ->
+          (* Comprador rechazó contraoferta *)
+          S.idle ep)
       (fun ep ->
-        let ep = Session.select_true ep in
-        Session.close ep)
+        (* Subastador acepta oferta *)
+        let ep = S.select_true ep in
+        S.close ep)
       ep
-  (*END*AuctionSeller*)
-
-  (*BEGIN*TestAuction*)
-  let test_buyer_seller ?(st = cst_placeholder) () =
-    let ep1, ep2 = Session.create ~st () in
-    let _ = Thread.create seller ep1 in
-    buyer ep2 42
-  (*END*TestAuction*)
-end
+  (*END*Auctioneer*)
